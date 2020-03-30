9d585366d5e1da63a472252ec5b5f1c79db97173
aldiwira <aldiwira.aw@gmail.com>
2020-03-30 13:23:42 +0700

    add transition at splash activity to main activity

--- app/src/main/java/com/aldi/project_uts/activities/Splash_activity.java
+++ app/src/main/java/com/aldi/project_uts/activities/Splash_activity.java
@@ -2,15 +2,30 @@ package com.aldi.project_uts.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.aldi.project_uts.R;
 
 public class Splash_activity extends AppCompatActivity {
+    private Intent intentHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        handleSplash(1000);
+    }
+    //class untuk splash screen agar dengan waktu yang dapat ditentukan lewat constructor
+    private void handleSplash(int time){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                intentHandler = new Intent(Splash_activity.this, MainActivity.class);
+                startActivity(intentHandler);
+                finish();
+            }
+        }, time);
     }
 }
