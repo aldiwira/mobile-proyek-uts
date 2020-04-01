4d5c31ca5bd34c2d93058213ac6f7114b1ddc209
aldiwira <aldiwira.aw@gmail.com>
2020-04-01 15:54:25 +0700

    change RV Model and add more function for handle transaction

--- app/src/main/java/com/aldi/project_uts/activities/MainActivity.java
+++ app/src/main/java/com/aldi/project_uts/activities/MainActivity.java
@@ -1,31 +1,38 @@
 package com.aldi.project_uts.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.aldi.project_uts.R;
-import com.aldi.project_uts.adapters.Transaction_Adapter;
+import com.aldi.project_uts.adapters.TransactionAdapter;
+import com.aldi.project_uts.models.Account;
 import com.aldi.project_uts.models.Transaction;
-import com.mikepenz.fastadapter.FastAdapter;
-import com.mikepenz.fastadapter.adapters.ItemAdapter;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    TextView dateText;
-    String date;
-    String time;
+public class MainActivity extends AppCompatActivity implements TransactionAdapter.OnItemTransactionListener {
+    public static final String TRANSACTION_KEY = "TRANSACTION";
+    public static final String INDEX_KEY = "INDEX";
+    public static final int INSERT_REQUEST = 1;
+    public static final int UPDATE_REQUEST = 2;
+    private TextView dateText;
+    private String date;
+    private String time;
+    private Intent worldOfIntent;
+
+    private TransactionAdapter transactionAdapter;
+    private RecyclerView transactionsView;
+    private Account account;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,16 +50,40 @@ public class MainActivity extends AppCompatActivity {
         time = new SimpleDateFormat("hh:mm a").format(calendar.getTime());
         dateText.setText(date);
     }
+
     private void setupRV(){
-        RecyclerView transactionView = findViewById(R.id.rv_home);
-        ItemAdapter itemAdapter = new ItemAdapter<>();
-        FastAdapter fastAdapter = FastAdapter.with(itemAdapter);
-        List transaction = new ArrayList<>();
-        transaction.add(new Transaction_Adapter("2", "2", date, time));
-        transaction.add(new Transaction_Adapter("3", "3", date, time));
-        itemAdapter.add(transaction);
-        transactionView.setAdapter(fastAdapter);
+        transactionsView = findViewById(R.id.rv_home);
+        transactionAdapter = new TransactionAdapter(account.getTransactions(),this);
+        transactionsView.setAdapter(transactionAdapter);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        transactionView.setLayoutManager(layoutManager);
+        transactionsView.setLayoutManager(layoutManager);
+    }
+
+    public void handleAddTransaction(View view) {
+        worldOfIntent = new Intent(MainActivity.this, Add_Data_Activity.class);
+        worldOfIntent.putExtra(TRANSACTION_KEY, new Transaction());
+        startActivityForResult(worldOfIntent, INSERT_REQUEST);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            Transaction transaction = data.getParcelableExtra(TRANSACTION_KEY);
+            if (requestCode == INSERT_REQUEST){
+                account.addTransactions(transaction);
+            }
+            transactionAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onTransactionClicked(int index, Transaction item) {
+        worldOfIntent = new Intent(MainActivity.this, Add_Data_Activity.class);
+        worldOfIntent.putExtra(TRANSACTION_KEY, item);
+        worldOfIntent.putExtra(INDEX_KEY, index);
+        startActivityForResult(worldOfIntent, UPDATE_REQUEST);
+        finish();
     }
 }
