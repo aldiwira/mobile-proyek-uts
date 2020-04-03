3c47863e89749c8b552d21779597d33223ab371a
aldiwira <aldiwira.aw@gmail.com>
2020-04-03 15:17:39 +0700

    add insert and update data barang with RV too

--- app/src/main/java/com/aldi/project_uts/activities/Add_Data_Activity.java
+++ app/src/main/java/com/aldi/project_uts/activities/Add_Data_Activity.java
@@ -1,6 +1,9 @@
 package com.aldi.project_uts.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,18 +11,30 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.aldi.project_uts.R;
+import com.aldi.project_uts.adapters.BarangAdapter;
+import com.aldi.project_uts.models.Barang;
 import com.aldi.project_uts.models.Transaction;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class Add_Data_Activity extends AppCompatActivity {
+public class Add_Data_Activity extends AppCompatActivity implements BarangAdapter.OnItemDataListener {
+    public static final String BARANG_KEY = "BARANG";
+    public static final String INDEX_KEY = "INDEX";
+    public static final int INSERT_REQUEST = 1;
+    public static final int UPDATE_REQUEST = 2;
+
     private String date;
     private String time;
     private int index;
+    private int sum_in_item = 0;
+    private int sum_out_item = 0;
 
+    private RecyclerView recordView;
+    private BarangAdapter adapter;
     private Transaction record;
     private Intent intent;
+
 
     private TextView dateText;
     @Override
@@ -29,6 +44,8 @@ public class Add_Data_Activity extends AppCompatActivity {
         record = new Transaction();
         bindSetup();
         setDateData();
+        getParcelableData();
+        setupRV();
     }
     private void bindSetup(){
         dateText = findViewById(R.id.header_date_text);
@@ -47,9 +64,15 @@ public class Add_Data_Activity extends AppCompatActivity {
             dateText.setText(record.getDate());
         }
     }
+    private void setupRV(){
+        recordView = findViewById(R.id.rv_data);
+        adapter = new BarangAdapter(record.getItems(), this);
+        recordView.setAdapter(adapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recordView.setLayoutManager(layoutManager);
+    }
     public void handleSaveRecord(View view) {
-        int in_data = 2;
-        int out_data = 2;
+
         record.setSumBarangMasuk(2);
         record.setSumBarangKeluar(2);
         record.setDate(date);
@@ -59,5 +82,39 @@ public class Add_Data_Activity extends AppCompatActivity {
         intent.putExtra(MainActivity.INDEX_KEY, index);
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+    @Override
+    public void onDataClicked(int index, Barang item) {
+        intent = new Intent(this, Add_Item_Activity.class);
+        intent.putExtra(BARANG_KEY, item);
+        intent.putExtra(INDEX_KEY, index);
+        startActivityForResult(intent, UPDATE_REQUEST);
+    }
+
+    public void handleAddBarang(View view) {
+        intent = new Intent(Add_Data_Activity.this, Add_Item_Activity.class);
+        intent.putExtra(BARANG_KEY, new Barang());
+        startActivityForResult(intent, INSERT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_OK){
+            Barang barang = data.getParcelableExtra(BARANG_KEY);
+            if (barang.getStatus() == Barang.Status.MASUK){
+                sum_in_item += 1;
+            } else if(barang.getStatus() == Barang.Status.KELUAR){
+                sum_out_item += 1;
+            }
+            if (requestCode == INSERT_REQUEST ){
+                record.addBarang(barang);
+            } else if(requestCode == UPDATE_REQUEST){
+                int index = data.getIntExtra(INDEX_KEY, 0);
+                record.updateBarang(barang, index);
+            }
+            adapter.notifyDataSetChanged();
+        }
     }
 }
